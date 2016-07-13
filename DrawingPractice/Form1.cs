c61806a6f5b396a23d8d63d5e2a6cf71db6521d2
Jennyf19 <ferriesj@gmail.com>
2016-07-13 09:40:19 -0700

    drawing shapes

---
+++ DrawingPractice/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 1);
+            Pen greenPen = new Pen(Color.FromArgb(255, 0, 255, 0), 10);
+            greenPen.Alignment = PenAlignment.Inset;
+
+            //Draw the line with the wide green pen;
+            e.Graphics.DrawRectangle(greenPen, 10, 100, 50, 50);
+
+            //Draw the line with the thin black pen
+            e.Graphics.DrawRectangle(blackPen, 10, 100, 50, 50);
+        }
+    }
+}
